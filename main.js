a61741c7c140f2acc0df80498d5a2c07451d28b7
atemp678 <ajt2158@columbia.edu>
2021-12-03 09:56:33 +0000

    plan for design

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ function handleRecipeClick() {}
 function handleInputChange() {}
 
 async function fetchRecipe(food) {}
+
+// General Ideas
+// We are making a receipe generator app, user design principles. 
+// Options of categories, cakes, chinese, pizza, korean, sushi-> get a random reciepe from api database
+// Recent articles about food at the bottom of the screen.
+
