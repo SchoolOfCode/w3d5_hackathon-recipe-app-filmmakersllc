dec9b00e82c2c5646e6f9bb098095f3e09343f50
Alex Mulgrew <alexmulgrew@Chloes-iMac.local>
2021-12-03 11:29:27 +0000

    added nav bar

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ async function fetchRecipe(food) {}
 // Options of categories, cakes, chinese, pizza, korean, sushi-> get a random reciepe from api database
 // Recent articles about food at the bottom of the screen.
 
+
+//4 Buttons created in a .navBar
+//Use .navBar as the parent element to assign the CSS to the navBar
+    //Home, returns to main page 
+    //Previous Recipes, list of previous recipes
+    //Im feeling lucky, Random from any cuisine
+    //Dad button, random ready meals (if time)
+
+
+//Buttons on main page to give them the cuisine options of choice
+    // 5 buttons showing our chosen cuisines
+    // Italian, British, Japanese, Indian and Persian
+
+//These buttons will take them to a new page where the recipe will show
+    //link a seperate html file, with the same design
+    //Only thing presented is the Random Recipe that is generated.
+    //Do this using the method below
+    //Fetch recipe API Randomly, cuisineType based on what they click
+
+
